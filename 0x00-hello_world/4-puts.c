b175aa6b1126415b64e2b4843b85ad9c00ed7780
Henock D <henocktuna9@gmail.com>
2022-09-11 06:14:52 -0700

    C - 4(fix)

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
-int main(void) {
-    char p[] = "\"Programming is like building a multilingual puzzle";
-    puts(p);
-    return 0;
+int main(void)
+{
+char p[] = "\"Programming is like building a multilingual puzzle";
+puts(p);
+return (0);
 }
